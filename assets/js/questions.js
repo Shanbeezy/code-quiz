af6f964bc929990956d182eeca054acc9fe09136
Shannon James Jr. <jay.are.sj@gmail.com>
2024-03-04 21:57:26 -0500

    finished js questions

---
+++ assets/js/questions.js
@@ -0,0 +1,52 @@
+var questions = [
+    {
+        title: 'What tag is required in all HTML documents, and is used to define the title?' ,
+        choices:['<head></head>', '<br><br/>', '<body><body/>', '<title><title/>'] ,
+        answer: '<head></head>' ,
+    },
+    {
+        title: 'What tag is used to render or transform text into an important (bold) version?' ,
+        choices:['blockquote>', '<a>', '<strong>', '<em>'] ,
+        answer: '<strong>' ,
+    },
+    {
+        title: 'In JavaScript, what element is used to store and manipulate text, usually in multiples?' ,
+        choices:['Arrays', 'Recorders', 'Strings', 'Variables'] ,
+        answer: 'Strings' ,
+    },
+    {
+        title:'When you are working with jQuery, you are in fact using what programming language?' ,
+        choices:['Python', 'Java', 'C++', 'JavaScript'] ,
+        answer: 'JavaScript' ,
+    },
+    {
+        title: 'What tag is used to specify a section of text that provides an example of computer code?' ,
+        choices:['<embed>', '<!DOCTYPE>', '<code>', '<caption>'] ,
+        answer: '<!DOCTYPE>' ,
+    },
+    {
+        title: 'What is the name of the stylesheet that defines the presentation of an HTML or XML document?' ,
+        choices:['Java', 'CSS', 'PHP', 'jQuery'] ,
+        answer: 'CSS' ,
+    },
+    {
+        title: 'What tag is used to define an interactive field where users can enter data?' ,
+        choices:['<enterpoint>', '<dialog>', '<input>', '<datalist>'] ,
+        answer: '<input>' ,
+    },
+    {
+        title: 'What are the CSS properties that are used to add space around sections of content?' ,
+        choices:['Padding', 'Cleaner', 'Break', 'Spacing'] ,
+        answer: 'Padding' ,
+    },
+    {
+        title: 'What is considered to be the most popular programming language in the world?' ,
+        choices:['HTML', 'JavaScript', 'Swift', 'Ruby'] ,
+        answer: 'JavaScript' ,
+    },
+    {
+        title: 'What is the element used – and hidden – in code that explains things and makes the content more readable?' ,
+        choices:['Comparisons', 'Notes', 'Comments', 'Quotations'] ,
+        answer: 'Comments' ,
+    },
+]
